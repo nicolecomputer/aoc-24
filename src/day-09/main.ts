b6b94235db23a70af63ca8c768962e0f754befde
Nicole Watts <76132106+nicolecomputer@users.noreply.github.com>
2024-12-09 15:50:44 -0500

    rough draft of compact

--- src/day-09/main.ts
+++ src/day-09/main.ts
@@ -76,9 +76,102 @@ function visualizeFileMap(map: DiskMap): string {
     return map.map(visualizeMapItem).join("")
 }
 
+function findLastIndex<T>(items: T[], predicate: (item: T) => boolean): number {
+    for (let i = items.length - 1; i > 0; i--) {
+        if (predicate(items[i])) {
+            return i
+        }
+    }
+    return -1
+}
+
+function isCompacted(map: DiskMap): boolean {
+    return false
+}
+
+function compact(map: DiskMap): DiskMap {
+    let nextMap = [...map]
+
+    const freeSpotIndex = map.findIndex(s => s.type === "freespace" && s.length > 0)
+    const freeSpot = map[freeSpotIndex]
+
+    const fileToMoveIndex = findLastIndex(map, s => s.type === "file" && s.length > 0)
+    const fileToMove = map[fileToMoveIndex]
+
+    nextMap[freeSpotIndex] = {
+        ...freeSpot,
+        length: freeSpot.length - 1
+    }
+    nextMap[fileToMoveIndex] = {
+        ...fileToMove,
+        length: fileToMove.length - 1
+    }
+    nextMap.splice(freeSpotIndex, 0, {
+        ...fileToMove,
+        length: 1
+    })
+    nextMap.push({
+        type: "freespace",
+        length: 1
+    })
+
+
+    // Cleanup
+    // - Remove empty blank spots / empty data
+    // - Join neighboring data types
+    nextMap = removeEmptySpots(nextMap) //?
+    nextMap = joinNeighbors(nextMap)
+    return nextMap
+}
+
+function isJoinable(a: DiskMapItem, b: DiskMapItem): boolean {
+    if (a.type === "freespace" && b.type === "freespace") {
+        return true
+    }
+    if (a.type === "file" && b.type == "file" && a.id == b.id) {
+        return true
+    }
+    return false
+}
+
+function joinNeighbors(map: DiskMap): DiskMap {
+    let nextMap = [...map]
+    for (let i = 1; i < nextMap.length; i++) {
+        const a = nextMap[i - 1]
+        const b = nextMap[i]
+        if (isJoinable(a, b)) {
+            console.log(a, b)
+            nextMap[i] = {
+                ...a,
+                length: a.length + b.length
+            }
+            nextMap.splice(i - 1, 1)
+        }
+    }
+    return nextMap
+}
+
+function removeEmptySpots(map: DiskMap): DiskMap {
+    return map.filter(s => s.length > 0)
+}
+
 function part1(input: string): number {
-    const diskmap = parseDiskMap(input) //?
-    visualizeFileMap(diskmap) //?
+    const diskmap = parseDiskMap(input)
+
+    let a = diskmap
+    visualizeFileMap(a) //?
+    a = compact(a)
+    visualizeFileMap(a) //?
+    a = compact(a)
+    visualizeFileMap(a) //?
+    a = compact(a)
+    visualizeFileMap(a) //?
+    // a = compact(a)
+    // visualizeFileMap(a) //?
+    // a = compact(a)
+    // visualizeFileMap(a) //?
+
+    a //?
     return 0
 }
 
